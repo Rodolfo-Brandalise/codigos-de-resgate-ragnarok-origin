6352b289ea3b3974073b568bd1b96cee38be8c1f
Rodolfo Nondilo Brandalise <rnbrandalise@gmail.com>
2024-03-24 16:09:36 -0300

    mudar botao de cor, arcangeling dentro de cada botao

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+function copyToClipboard(texto) {
+    navigator.clipboard.writeText(texto);
+
+    const clickedButton = document.querySelector('.botao:focus');
+    if (clickedButton) {
+    clickedButton.classList.add('botao-clicado');
+    }
+}
