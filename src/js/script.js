abb2913a7c011a42a0893aad39d96c3ef6f0ca8d
Rodolfo Nondilo Brandalise <rnbrandalise@gmail.com>
2024-03-26 10:06:53 -0300

    adicionada novamente a função de trocar o botão de cor

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,13 @@
-function copyToClipboard(texto) {
-    navigator.clipboard.writeText(texto);
+import codigos from './codigos.js';
+const botoes = document.getElementById('botoes');
+codigos.forEach(i => botoes.innerHTML += `<button class="botao"><img src="src/imagens/bau.png" alt=${i}></button>`);
+botoes.addEventListener('click', (event) => copyToClipboard(event.target.alt));
+
+function copyToClipboard(codigo) {
 
     const clickedButton = document.querySelector('.botao:focus');
-    if (clickedButton) {
-    clickedButton.classList.add('botao-clicado');
-    }
+    if (clickedButton && codigo) {
+        navigator.clipboard.writeText(codigo);
+        return clickedButton.classList.add('botao-clicado')
+    };
 }
-
